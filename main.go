8d4c06d8751c90b323a1506b12c89fffa59d6307
Brayan Muniz <35124364+brayanMuniz@users.noreply.github.com>
2024-12-30 17:24:38 -0600

    added a json req/res example

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"sync/atomic"
@@ -25,6 +26,20 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	})
 }
 
+// to keep code DRY
+func writeJSONResponse(w http.ResponseWriter, statusCode int, respBody interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+
+	dat, err := json.Marshal(respBody)
+	if err != nil {
+		w.WriteHeader(500) // Internal server error
+		w.Write([]byte(`{"error": "Internal server error"}`))
+		return
+	}
+	w.Write(dat)
+}
+
 func main() {
 	mux := http.NewServeMux() // responsible for handling and routing paths
 	server := http.Server{
@@ -41,24 +56,49 @@ func main() {
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(handler))
 
 	// metrics
-	mux.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		msg := (fmt.Sprintf("Hits: %d", apiCfg.fileserverHits.Load()))
+	mux.HandleFunc("GET /admin/metrics", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		msg := (fmt.Sprintf("<html> <body> <h1>Welcome, Chirpy Admin</h1> <p>Chirpy has been visited %d times!</p> </body> </html>", apiCfg.fileserverHits.Load()))
 		w.Write([]byte(msg))
 	})
 
 	// reset
-	mux.HandleFunc("/reset", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("POST /admin/reset", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(200)
 		apiCfg.fileserverHits.Store(0)
 	})
 
 	// healthz
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(200)
 		msg := []byte("OK")
 		w.Write(msg)
+	})
+
+	// /api/validate_chirp
+	mux.HandleFunc("POST /api/validate_chirp", func(w http.ResponseWriter, r *http.Request) {
+		type parameters struct {
+			Body string `json:"body"` // NOTE: this must be capatilized in order to be exported, the value on the right is the name of what goes out
+		}
+		decoder := json.NewDecoder(r.Body)
+		params := parameters{}
+		err := decoder.Decode(&params)
+		if err != nil {
+			writeJSONResponse(w, 500, map[string]string{"error": "Something went wrong"})
+			return
+		}
+
+		// msg too long
+		if len(params.Body) > 140 {
+			writeJSONResponse(w, 400, map[string]string{"error": "Chirp is too long"})
+			return
+		}
+
+		// message is good
+		writeJSONResponse(w, 200, map[string]bool{"valid": true})
+		return
+
 	})
 
 	server.ListenAndServe()
