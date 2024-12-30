5cea2f80237d504920f35e2c1fbd4c7d297477d4
Brayan Muniz <35124364+brayanMuniz@users.noreply.github.com>
2024-12-30 16:03:47 -0600

    added middleware to check how many time the static files are being used

--- main.go
+++ main.go
@@ -1,6 +1,29 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+// this is the method in order to increment the apiConfig by one
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	// Return a NEW handler
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// This is where we can do things BEFORE handling the request
+		cfg.fileserverHits.Add(1)
+
+		// This is how we call the next handler
+		next.ServeHTTP(w, r)
+
+		// This is where we can do things AFTER handling the request
+
+	})
+}
 
 func main() {
 	mux := http.NewServeMux() // responsible for handling and routing paths
@@ -9,9 +32,26 @@ func main() {
 		Handler: mux,
 	} // listens to network address and handles it with mux
 
+	apiCfg := apiConfig{} // this inits it to its 0 value
+
 	// Serve static files from the current directory under the /app/ path
 	// StripPrefix removes /app from the request path before looking for files
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("./"))))
+	fileServer := http.FileServer(http.Dir("./"))
+	handler := http.StripPrefix("/app", fileServer)
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(handler))
+
+	// metrics
+	mux.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		msg := (fmt.Sprintf("Hits: %d", apiCfg.fileserverHits.Load()))
+		w.Write([]byte(msg))
+	})
+
+	// reset
+	mux.HandleFunc("/reset", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		apiCfg.fileserverHits.Store(0)
+	})
 
 	// healthz
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
